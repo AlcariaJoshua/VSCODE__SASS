1b77271d2f3c38df85cf677d037c42f9bbffa277
joshualcaria <alcariajoshua13@gmail.com>
2023-03-22 15:46:53 +0800

    header

--- dist/javascript/script.js
+++ dist/javascript/script.js
@@ -1,26 +1,8 @@
-const toggle = document.querySelector(".toggle__menu");
-const nav = document.querySelector(".header__nav");
+const burger__btn = document.querySelector(".burger__btn");
+const navigation = document.querySelector(".navigation");
+burger__btn.addEventListener("click", () => {
+    burger__btn.classList.toggle("open");
+    navigation.classList.toggle("open");
+    navigation.style=("transition: .5s ease")
 
-
-
-toggle.addEventListener("click", () => {
-    nav.classList.toggle("open");
-    toggle.classList.toggle("open");
-})
-
-// new
-
-const headerB = document.querySelector(".header");
-
-window.addEventListener("scroll", () => {
-    console.log(window.pageYOffset);
-    if(window.pageYOffset >= 100){
-        headerB.classList.remove("shadow");
-    }
-    else{
-        headerB.classList.add("shadow");
-    }
 });
-
-
-
